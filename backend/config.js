977533e9f104c0ecccacfac97d1deb980786c706
OnyiKaroli <karolionyango@gmail.com>
2024-06-11 08:36:07 +0300

    Backend in the works

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://karolionyango:mendoza#2024@bookstore.xycp0t8.mongodb.net/bookStore?retryWrites=true&w=majority&appName=bookStore'
---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+
+const app = express();
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Welcome to BookStore!');
+});
+
+app.listen(PORT, () => {
+    console.log(`App is listening to port: ${PORT}`);
+});
